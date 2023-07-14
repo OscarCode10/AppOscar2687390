5a18011a9f7f2a6d64f989b1dbc8e336d1b62322
OscarCode10 <oscaortiz10@gmail.com>
2023-07-14 16:29:55 -0500

    while

--- app/src/main/java/com/OscarCode10/myapplication/while.kt
+++ app/src/main/java/com/OscarCode10/myapplication/while.kt
@@ -1,41 +1,13 @@
 package com.OscarCode10.myapplication
 
-
-/*fun main(){
-    for (i in 1 .. 10){
-        var control:Int=i%2
-        if (control==0) {
-            println("El $i es número par")
-        }
-        else{
-            println("El $i es número impar")
-        }
-    }
-}*/
-
-/*fun main(){
-    var numPar:Int=0
-    var numImpar:Int=0
-    for (i in 1 .. 10){
-        println("digite un número")
-        var deci:Int=readLine()!!.toInt()
-        var control:Int=deci%2
-        if (control==0) {
-            numPar+=1
-        }
-        else{
-            numImpar+=1
-        }
-    }
-    println("Los números pares son $numPar y los números impares son $numImpar ")
-}*/
-
 fun main(){
+
     var numPar:Int=0
     var numImpar:Int=0
     var numParMas:Int=0
     var numImparMas:Int=0
-    for (i in 1 .. 10){
+    var op:String="si"
+    while(op=="si") {
         println("digite un número")
         var deci:Int=readLine()!!.toInt()
         var control:Int=deci%2
@@ -51,6 +23,8 @@ fun main(){
             numImparMas+=deci
             numImpar+=1
         }
+        println("Deseas digitar otro número")
+        op= readLine().toString()
     }
     println("La suma de los numeros pares es $numParMas y la suma de los numeros impares es $numImparMas")
     println("Los números pares son $numPar y los números impares son $numImpar ")
